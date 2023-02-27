c57ce4499813c0749357c8c432d470ac2f903a24
JoshSamad <joshsamadder@arizona.edu>
2023-02-26 18:27:00 -0700

    Changed the song that is played

--- MediaFX/src/PlaySongDemo/PlayAnMP3.java
+++ MediaFX/src/PlaySongDemo/PlayAnMP3.java
@@ -28,7 +28,9 @@ public class PlayAnMP3 extends Application {
   @Override
   public void start(Stage stage) throws Exception {
     BorderPane pane = new BorderPane();
-    String path = "songfiles/Capture.mp3";
+    
+    // I, Josh Samadder, changed the song to be played before pushing to Github
+    String path = "songfiles/DanseMacabreViolinHook.mp3";
     pane.setCenter( new Label(path));
     playASong(path);
     // Put the pane in a sized Scene and show the GUI
